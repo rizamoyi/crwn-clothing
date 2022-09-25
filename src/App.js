3385088d00190c5a341cb2f121d15db646662249
rizamoyi <samwelmoyi@gmail.com>
2022-09-25 23:52:57 +0300

    added redux react-redux redux-logger, created store and migrated user context to redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
 
 import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
@@ -6,7 +13,22 @@ import Checkout from './routes/checkout/checkout.component';
 import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
 
+import { setCurrentUser } from './store/user/user.action';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -15,6 +15,10 @@ export const USER_ACTION_TYPES = {
   SET_CURRENT_USER: 'SET_CURRENT_USER',
 };
 
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 const userReducer = (state, action) => {
   const { type, payload } = action;
 
@@ -27,10 +31,6 @@ const userReducer = (state, action) => {
     default:
       throw new Error(`Unhandled type ${type} in userReducer`);
   }
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
 };
 
 export const UserProvider = ({ children }) => {
