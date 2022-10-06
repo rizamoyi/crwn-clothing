9e1e4b67d48216ec2bf68189fc7ea341798cd4f9
rizamoyi <samwelmoyi@gmail.com>
2022-10-06 22:24:55 +0300

    migrate user auth to use redux saga workflows

--- src/App.js
+++ src/App.js
@@ -2,33 +2,20 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from './utils/firebase/firebase.utils';
-
 import Authentication from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
 import Checkout from './routes/checkout/checkout.component';
 import Home from './routes/home/home.component';
 import Shop from './routes/shop/shop.component';
 
-import { setCurrentUser } from './store/user/user.action';
+import { checkUserSession } from './store/user/user.action';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
-    // added only to silence warning but not neccesarry
-  }, [dispatch]);
+    dispatch(checkUserSession());
+  }, []);
 
   return (
     <Routes>
